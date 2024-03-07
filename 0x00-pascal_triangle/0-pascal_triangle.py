70f2ee56aa4549a09e3e40da92bb08e9b78107ed
ekarkat <ekarkat@gmail.com>
2024-03-07 12:16:33 +0000

    pasacal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+def pascal_triangle(n):
+    if n == 0:
+        return []
+    pascal_list = []
+    for i in range(n):
+        row = []
+        len_p = len(pascal_list)
+        for j in range(i + 1):
+            if j == 0:
+                row.append(1)
+            if (j == len_p and j != 0):
+                row.append(1)
+                break
+            if (j < len_p and j != 0):
+                last_row = pascal_list[len_p - 1]
+                row.append(last_row[j-1] + last_row[j])
+
+        pascal_list.append(row)
+
+    return pascal_list

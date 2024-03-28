3f1efdb27fa7313c4b6f9b9862bcfe8846b2d089
ekarkat <ekarkat@gmail.com>
2024-03-28 22:29:58 +0000

    remoce check

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,9 +7,9 @@ import sys
 def print_out(dic, size):
     """Print function"""
     print("File size: {}".format(size))
-    for key in sorted(dic.keys()):
+    for key, value in sorted(dic.items()):
         if dic[key] != 0:
-            print("{}: {}".format(key, dic[key]))
+            print("{}: {}".format(key, value))
 
 
 status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
@@ -22,16 +22,14 @@ try:
     for line in sys.stdin:
         count += 1
         parts = line.split()
-        if len(parts) == 9:
-            if parts[7].isdigit and parts[8].isdigit:
-                try:
-                    status_codes[parts[7]] += 1
-                except Exception:
-                    pass
-                try:
-                    file_size = file_size + int(parts[8])
-                except Exception:
-                    pass
+        try:
+            status_codes[parts[7]] += 1
+        except Exception:
+            pass
+        try:
+            file_size = file_size + int(parts[8])
+        except Exception:
+            pass
         if count % 10 == 0:
             print_out(status_codes, file_size)
 

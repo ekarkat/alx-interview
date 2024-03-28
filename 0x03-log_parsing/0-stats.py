6c5601e4c57e14fbc11d6c68feff888c32ac1e6d
ekarkat <ekarkat@gmail.com>
2024-03-28 23:23:55 +0000

    tasssssss

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,15 +3,6 @@
 
 import sys
 
-
-def print_out(status, size):
-    """Print function"""
-    print("File size: {}".format(size))
-    for key, value in sorted(status.items()):
-        if value != 0:
-            print("{}: {}".format(key, value))
-
-
 status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
                 '404': 0, '405': 0, '500': 0}
 
@@ -26,10 +17,16 @@ try:
             if parts[-2] in status_codes.keys() and parts[-1].isdigit:
                 status_codes[parts[-2]] += 1
                 file_size = file_size + int(parts[-1])
-        except:
+        except Exception:
             pass
         if count % 10 == 0 and file_size != 0 and count != 0:
-            print_out(status_codes, file_size)
+            print("File size: {}".format(file_size))
+            for key, value in sorted(status_codes.items()):
+                if value != 0:
+                    print("{}: {}".format(key, value))
 
 except KeyboardInterrupt:
-    print_out(status_codes, file_size)
+    print("File size: {}".format(file_size))
+    for key, value in sorted(status_codes.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))

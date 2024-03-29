d8641f2d53820a58a316164e2265dc97d98d7e94
ekarkat <ekarkat@gmail.com>
2024-03-29 12:43:53 +0000

    correction

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,6 +3,14 @@
 
 import sys
 
+
+def print_msg(file_size, status_codes):
+    print("File size: {}".format(file_size))
+    for key, value in sorted(status_codes.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
+
+
 status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
                 '404': 0, '405': 0, '500': 0}
 
@@ -20,13 +28,9 @@ try:
         except Exception:
             pass
         if count % 10 == 0 and file_size != 0 and count != 0:
-            print("File size: {}".format(file_size))
-            for key, value in sorted(status_codes.items()):
-                if value != 0:
-                    print("{}: {}".format(key, value))
+            print_msg(file_size, status_codes)
+    print_msg(file_size, status_codes)
+
 
 except KeyboardInterrupt:
-    print("File size: {}".format(file_size))
-    for key, value in sorted(status_codes.items()):
-        if value != 0:
-            print("{}: {}".format(key, value))
+    print_msg(file_size, status_codes)

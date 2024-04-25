ff61d341dacbe0cf9c8b031b2c48dcd2d790d9e2
ekarkat <ekarkat@gmail.com>
2024-04-25 23:18:38 +0100

    hard coding

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" 2DMatrix Rotate"""
+
+
+def rotate_2d_matrix(matrix):
+    """ Matrix Rotater """
+    n = len(matrix)
+    cmat = [[0] * n for _ in range(n)] 
+    for i in range(len(cmat)):
+        for j in range(len(cmat[i])):
+            cmat[i][j] = matrix[len(cmat) - j - 1][i]
+    i = 0
+    for row in cmat:
+        matrix[i] = row
+        i = i + 1
---
+++ 0x07-rotate_2d_matrix/test.py
@@ -0,0 +1,5 @@
+a = [15, 17]
+
+state = [[0] * 3 for _ in range(3)]  # Start with an empty board
+
+print(10//2)

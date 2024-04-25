53174bc7715f99570d3fe0925941d0ad97a78e07
ekarkat <ekarkat@gmail.com>
2024-04-25 23:21:20 +0100

    small modification

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,9 +6,9 @@ def rotate_2d_matrix(matrix):
     """ Matrix Rotater """
     n = len(matrix)
     cmat = [[0] * n for _ in range(n)]
-    for i in range(len(cmat)):
-        for j in range(len(cmat[i])):
-            cmat[i][j] = matrix[len(cmat) - j - 1][i]
+    for i in range(n):
+        for j in range(n):
+            cmat[i][j] = matrix[n - j - 1][i]
     i = 0
     for row in cmat:
         matrix[i] = row

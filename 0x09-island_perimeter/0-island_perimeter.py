cac50bd13210540f9ea1b2ef142132f5afe4b968
ekarkat <ekarkat@gmail.com>
2024-05-09 12:24:15 +0100

    attempt 1

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,20 +3,18 @@
 
 
 def island_perimeter(grid):
-    """ The function description """
-    p = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            try:
-                if grid[i][j] == 1:
-                    if grid[i - 1][j] == 0:
-                        p += 1
-                    if grid[i][j - 1] == 0:
-                        p += 1
-                    if grid[i][j + 1] == 0:
-                        p += 1
-                    if grid[i + 1 ][j] == 0:
-                        p += 1
-            except Exception:
-                pass
-    return p
+    """ function description """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
